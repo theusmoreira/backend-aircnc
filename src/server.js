af70b4505524570703f41625da6af10050596c6e
matheussantos-dev <matheussm301@gmail.com>
2019-10-01 23:43:47 -0300

    import mongoose

--- src/server.js
+++ src/server.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+
 const routes = require('./routes');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://teks:teks@cluster0-wfzcl.mongodb.net/test?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
 
 app.use(express.json());
 app.use(routes);
