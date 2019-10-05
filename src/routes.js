df9f5e6642c245844683b81b5149c68603ce7dd8
matheussantos-dev <matheussm301@gmail.com>
2019-10-04 23:32:46 -0300

    add multer

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,15 @@
 const express = require('express');
+const multer = require('multer');
+const uoloadConfig = require('./config/upload');
+
 const SessionController = require('./controllers/SessionController');
 const SpotController = require('./controllers/SpotController');
 
 const routes = express.Router();
+const upload = multer(uoloadConfig);
 
 routes.post('/sessions', SessionController.store);
 
-routes.post('/spots', SpotController.store);
+routes.post('/spots', upload.single('thumbnail'), SpotController.store);
 
 module.exports = routes;
