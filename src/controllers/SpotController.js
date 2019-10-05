ca4107038dae9cdb9958777c442246f242c976a6
matheussantos-dev <matheussm301@gmail.com>
2019-10-05 00:06:47 -0300

    new route

--- src/controllers/SpotController.js
+++ src/controllers/SpotController.js
@@ -1,11 +1,34 @@
+const User = require('../models/User');
 const Spot = require('../models/Spot');
 
 module.exports = {
-    async store(req, res) {
-        const { filename } = req.fie;
-        const { company, techs, price } = req.body;
-        const { user_id } =req.headers;
+    async index(req, res) {
+        const { tech } = req.query;
 
-        return res.json({ ok: true })
+        const spots = await Spot.find({ techs: tech });
+
+        return res.json(spots);
+    },
+
+    async store(req, res) {
+        const { filename } = req.file;
+        const { company, techs, price } = req.body;
+        const { user_id } = req.headers;
+
+        const user = await User.findById(user_id);
+
+        if (!user) {
+            return res.status(400).json({ error: 'User does not exits' });
+        }
+
+        const spot = await Spot.create({
+            user: user_id,
+            thumbnail: filename,
+            company,
+            techs: techs.split(',').map(tech => tech.trim()),
+            price
+        })
+
+        return res.json(spot)
     }
 };
--- src/routes.js
+++ src/routes.js
@@ -11,5 +11,6 @@ const upload = multer(uoloadConfig);
 routes.post('/sessions', SessionController.store);
 
 routes.post('/spots', upload.single('thumbnail'), SpotController.store);
+routes.get('/spots', SpotController.index);
 
 module.exports = routes;
